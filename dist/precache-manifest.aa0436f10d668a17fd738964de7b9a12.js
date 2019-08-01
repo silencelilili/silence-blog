95b8911cd010a11f074a3416fb88610ef56eea3b
lixd <lixd@svocloud.com>
2019-08-01 18:09:09 +0800

    提交build后的dist目录

---
+++ dist/precache-manifest.aa0436f10d668a17fd738964de7b9a12.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "00b6eafd4eb275bff815edd2ce67eab8",
+    "url": "/mock/tags.json"
+  },
+  {
+    "revision": "b35edfd43378765337e1e3239d7483ef",
+    "url": "/mock/articleClass.json"
+  },
+  {
+    "revision": "ace448769482e65ec68f8f6d3009e523",
+    "url": "/mock/article.json"
+  },
+  {
+    "revision": "8f7c9f2f2b97cf048277",
+    "url": "/js/chunk-vendors.5553971b.js"
+  },
+  {
+    "revision": "9f8462d0d335744288d5",
+    "url": "/js/app.8c5cdada.js"
+  },
+  {
+    "revision": "1a98ed1b541ce1f8147b",
+    "url": "/js/about.208eb305.js"
+  },
+  {
+    "revision": "efb2f229a344987afa412708694b3e74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "8f7c9f2f2b97cf048277",
+    "url": "/css/chunk-vendors.d9890480.css"
+  },
+  {
+    "revision": "9f8462d0d335744288d5",
+    "url": "/css/app.191647cf.css"
+  }
+];
